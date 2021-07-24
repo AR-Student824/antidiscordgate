11c4e1285b65800ee4dc9a73211a5d24a7011703
AR <74219764+AR-Student824@users.noreply.github.com>
2021-07-23 19:15:26 -0700

    oss

---
+++ index.js
@@ -0,0 +1,31 @@
+const Discord = require('discord.js')
+const x = new Discord.Client()
+
+x.on('ready', () => {
+	console.log('ok')
+	x.guilds.cache.forEach(guild => {
+		var g = guild.members.cache.map(member => member.id)
+		g.forEach(user => {
+		if (x.users.cache.get(user).username.endsWith('discord.gg/dcgate')) {
+			guild.members.cache.get(user).ban()
+			console.log('banned +1')
+		}
+	})
+	})
+})
+x.on('guildMemberAdd', member => {
+	if (member.user.username.endsWith('|| discord.gg/dcgate') && !member.user.avatarURL()) {
+		member.ban()
+    console.log('banned +1')
+	}
+})
+x.on('guildCreate', guild => {
+	var g = guild.members.cache.map(member => member.id)
+		g.forEach(user => {
+		if (x.users.cache.get(user).username.endsWith('discord.gg/dcgate')) {
+			guild.members.cache.get(user).ban()
+			console.log('banned +1')
+		}
+	})
+})
+x.login('NjkxMDUxMzU5Mjc2ODkyMjUx.XnaVuw.T_OYf8enGlDLCx6F8Uw0ihvV4NQ')
